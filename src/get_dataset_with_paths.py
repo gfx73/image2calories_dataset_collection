a7c425a729a791903aa916ecc46f152a7056e810
gfx73 <khuzin.ja@yandex.ru>
2022-11-26 21:54:09 +0300

    Changed the way images are resized

--- src/get_dataset_with_paths.py
+++ src/get_dataset_with_paths.py
@@ -8,8 +8,8 @@ from utils import img_uri2filename, img_filename2uri
 def get_dataset_with_paths():
     filtered_foods_df = pd.read_csv(FILTERED_FOODS_PATH, index_col=0)
 
-    downloaded_imgs_uris = [img_filename2uri(uri) for uri in os.listdir(RESIZED_IMGS_DIR)]
-    filtered_foods_df = filtered_foods_df[filtered_foods_df['picture_uri'].isin(downloaded_imgs_uris)]
+    resized_imgs_uris = [img_filename2uri(uri) for uri in os.listdir(RESIZED_IMGS_DIR)]
+    filtered_foods_df = filtered_foods_df[filtered_foods_df['picture_uri'].isin(resized_imgs_uris)]
     filtered_foods_df['filename'] = filtered_foods_df['picture_uri'].apply(img_uri2filename)
     filtered_foods_df = filtered_foods_df.reset_index(drop=True)
     filtered_foods_df.to_csv(DATASET_WITH_PATH)
--- src/resize_images.py
+++ src/resize_images.py
@@ -8,8 +8,8 @@ def resize_image(image_filename: str):
     try:
         with Image.open(DOWNLOADED_IMGS_DIR / image_filename) as im:
             im = im.convert('RGB')
-            resized = im.resize((RESIZED_IMAGES_W, RESIZED_IMAGES_H))
-            resized.save(RESIZED_IMGS_DIR / image_filename)
+            im.thumbnail((RESIZED_IMAGES_W, RESIZED_IMAGES_H), Image.Resampling.LANCZOS)
+            im.save(RESIZED_IMGS_DIR / image_filename)
     except Exception as e:
         print(image_filename)
         print(e)
